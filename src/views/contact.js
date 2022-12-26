7d1fa602ff2e55673f69db43c01ad35c1e2c72e5
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-12-26 20:19:58 +0200

    Change colors and add css variables

--- src/views/contact.js
+++ src/views/contact.js
@@ -3,7 +3,7 @@ import { html } from '../lib/lit-html.js';
 
 const contactTemplate = () => html`
 <section class="contact" id="contact">
-  <form action="">
+  <form>
     <p>Пишете ни: naturalshine_ltd@abv.bg</p>
     <input type="email" placeholder="email" class="box">
     <textarea name="" class="box" placeholder="message" id="" cols="30" rows="10"></textarea>
