61b28922356e25345e16f3ea9d2bea890d922c5d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-02-26 16:10:12 +0200

    Add voucher for gift

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -4,6 +4,7 @@ import { products } from '../api/data.js';
 
 const firstHeading = ['Baby shine - ', 'продукти за бебета'];
 const secondHeading = ['Natural shine - ', 'продукти за възрастни'];
+const thirdHeading = ['Natural shine - ', 'ваучери за подарък'];
 
 const cardTemplate = (p) => html`
 <div class="box">
@@ -25,6 +26,8 @@ const catalogTemplate = (products) => html`
     <div class="box-container">${products.filter(p => p.serie === 'babyshine').map(cardTemplate)}</div>
     <h1 class="heading"> ${secondHeading[0]} <span>${secondHeading[1]}</span> </h1>
     <div class="box-container">${products.filter(p => p.serie === 'naturalshine').map(cardTemplate)}</div>
+    <h1 class="heading"> ${thirdHeading[0]} <span>${thirdHeading[1]}</span> </h1>
+    <div class="box-container">${products.filter(p => p.serie === 'voucher').map(cardTemplate)}</div>
 </section>`;
 
 export function showCatalog(ctx) {
